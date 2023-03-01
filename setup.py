90f64f3f4071a46824b58f24d91f458890c3b222
huanyu <qhy20@mails.tsinghua.edu.cn>
2023-03-01 18:09:35 +0800

    build: update setup file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(DIR, 'README.md')) as f:
 
 setup(
     name="hnn",
-    version="0.0.3.1",
+    version="0.0.3.2",
     packages=find_packages(exclude=["examples", "examples.*", "unit_tests", "unit_tests.*"]),
     keywords=["hybrid neural networks", "spiking neural networks", "quantization"],
     description="A programming framework based on PyTorch for hybrid neural networks with automatic quantization",
